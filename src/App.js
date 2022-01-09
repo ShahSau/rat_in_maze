35ba88f6f510e48e5bcdd2bca11bbcc9243f8768
shahriar <shahriarksaurov@gmail.com>
2022-01-09 13:44:58 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Maze from './Components/Maze.js';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>I am a 🐀, take me 🏠</h1>
+      <Maze />
+    </div>
+  );
+}
+
+export default App;
